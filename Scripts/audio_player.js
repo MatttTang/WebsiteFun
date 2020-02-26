9d0eca2398d85a15c1af0690c5dc7265ae3f30f8
MatttTang <46803888+MatttTang@users.noreply.github.com>
2020-02-26 10:51:25 -0800

    Trying to play audio

---
+++ Scripts/audio_player.js
@@ -0,0 +1,10 @@
+var myMusic = document.getElementById("music");
+console.log(myMusic);
+function play(){
+    console.log("test");
+    myMusic.play();
+}
+
+function pause(){
+    myMusic.pause();
+}
